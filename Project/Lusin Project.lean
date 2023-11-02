72ad80580de15ad9d07e9b96fc8f27dfdcde7b43
GiovanniCrocioni <127878440+GiovanniCrocioni@users.noreply.github.com>
2023-11-02 18:20:24 +0000

    Lusin Project.lean

--- Project/Lusin Project.lean
+++ Project/Lusin Project.lean
@@ -8,18 +8,23 @@ import Mathlib.MeasureTheory.Constructions.BorelSpace.Basic
 
 This file contains Lusin's theorem which put very simply and in the words of J. E. Littlewood,
 "every measurable function is nearly continuous". More precisely the statement of Lusin's theorem
-we will prove goes as follows: Let f be a  measureable function from a Euclidean space equipped with
-the Borel sigma-algebra to the Reals equipped with the Borel sigma-algebra. For any set A in the
-first sigma-algebra with finite  measure, there exists a compact set K, such that f is continuous on
+we will prove goes as follows: Let f be a  measureable function from a locally compact Hausdorff
+space to the Reals, both equipped with the Borel sigma-algebra. For any set A in the first  
+sigma-algebra with finite  measure, there exists a compact set K, such that f is continuous on
 K and the measure of A \ K is arbitrarily small.
 
 !-/
 
+
 noncomputable section
+
+--open MeasureTheory
 
 variable {α β : Type*} {m : MeasurableSpace α} [MetricSpace β] {μ : MeasureTheory.Measure α}
 
 
 variable {f : α  → β }
 
-namespace Lusin
+--instance Real.measurableSpace : MeasurableSpace ℝ  := borel ℝ 
+
+
