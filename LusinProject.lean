9904c18159055008c903650d573cf1cf2237dc23
GiovanniCrocioni <127878440+GiovanniCrocioni@users.noreply.github.com>
2023-11-09 18:51:08 +0000

    Delete LusinProject.lean

--- LusinProject.lean
+++
@@ -1,5 +0,0 @@
-
-
-theorem LusinonT2LocallyCompactSpace [measure.regular μ]
- ∃ t, MeasurableSet t ∧ CompactSet t ∧ μ Set.diff a \ t ≤ ENNReal.ofReal ε ∧
-  Continuous Set.Restrict f t  := by sorry
