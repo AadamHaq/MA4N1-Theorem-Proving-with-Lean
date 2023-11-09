c30dcd2b4e3a01e1c01e7680854a690fb1df2789
GiovanniCrocioni <127878440+GiovanniCrocioni@users.noreply.github.com>
2023-11-09 18:49:57 +0000

    Create LusinProject.lean
    First attempt at statement

---
+++ LusinProject.lean
@@ -0,0 +1,5 @@
+
+
+theorem LusinonT2LocallyCompactSpace [measure.regular μ]
+ ∃ t, MeasurableSet t ∧ CompactSet t ∧ μ Set.diff a \ t ≤ ENNReal.ofReal ε ∧
+  Continuous Set.Restrict f t  := by sorry
