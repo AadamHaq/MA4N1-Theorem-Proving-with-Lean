1998666f034f6bcdb00792265b97162b45f189cb
AmeerAliSaleem <116832927+AmeerAliSaleem@users.noreply.github.com>
2023-11-10 13:01:19 +0000

    Add files via upload
    Added some descriptions of preliminary theorems we need to prove/invoke before tackling the main problem.

--- Project/T2LocallyCompactSpace.lean
+++ Project/T2LocallyCompactSpace.lean
@@ -17,7 +17,6 @@ open scoped Topology
 
 variable {α β ι : Type*} {m : MeasurableSpace α} [n : MetricSpace β] {μ : Measure α}
 
-
 -- aim is to prove Lusin's Theorem for the Borel sigma algebra specifically
 -- this is slightly more restrictive than the theorem in Cohn's book
 
@@ -27,7 +26,7 @@ class T2LocallyCompactSpace (α : Type*) [TopologicalSpace α] [T2Space α] [Loc
 instance (α : Type*) [TopologicalSpace α] [T2Space α] [LocallyCompactSpace α] :
     T2LocallyCompactSpace α := {}
 
-def T2LocallyCompactSpaceBorel (X : T2LocallyCompactSpace α) (m : MeasurableSpace α) :=
+def T2LocallyCompactSpaceBorel (T2LocallyCompactSpace α) (m : MeasurableSpace α) :=
 BorelSpace X
 
 /-
@@ -38,6 +37,35 @@ class BorelSpace (α : Type*) [TopologicalSpace α] [MeasurableSpace α] : Prop 
     measurable_eq : ‹MeasurableSpace α› = borel α
 -/
 
+/-
+Theorem: the pre-image of a singleton under f measurable is measurable
+-/
+
+-- write theorem here
+
+/-
+Theorem: the countable union of compact sets is compact.
+-/
+
+-- write theorem here
+
+/-
+Proposition 1.2.5 in Cohn's book [Continuity of Measure]: μ(⋃ A_k) = lim μ(A_k) for an increasing sequence of sets {A_k} with A = ⋃ A_k
+-/
+
+-- write proposition here
+
+/- Lusin's Theorem for measurable f which takes **countably many values**. Suppose that f takes countable many values {a_1,...,a_k}. Define A_k = f^-1(a_k) for each k, with A = ⋃ A_k.
+
+Now apply Prop 1.2.5 to show that lim μ(⋃ A_k) = μ(A). Since μ is regular, μ(A) < ∞ and so the measure of A \ ⋃_k^n A_k is arbitrarily small.
+
+We now apply the regularity of μ to apply a similar argument, only this time with compact K = ⋃ K_k for K_k compact.
+-/
+
+-- write countable f theorem here
+
+
+-- Lusin's Theorem!
 theorem lusin {X : T2LocallyCompactSpace α} [Measure.Regular μ]
 
 #check Field
