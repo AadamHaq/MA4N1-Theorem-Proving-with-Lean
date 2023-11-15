fc303e588d18c0ef53307d7913538b753323559e
AmeerAliSaleem <116832927+AmeerAliSaleem@users.noreply.github.com>
2023-11-15 21:24:38 +0000

    Add files via upload
    Added some further commented breakdowns of the theorems needed to prove the main theorem.

--- Project/T2LocallyCompactSpace.lean
+++ Project/T2LocallyCompactSpace.lean
@@ -15,6 +15,12 @@ open scoped Topology
 
 -- namespace MeasureTheory
 
+/-
+--- PRELIMINARY INFO---
+
+For simplicity, we restrict our attention to the Borel σ-algebra: this differs slightly from Cohn's measure theory book, which accounts for any σ-algebra containing the Borel σ-algebra.
+-/
+
 variable {α β ι : Type*} {m : MeasurableSpace α} [n : MetricSpace β] {μ : Measure α}
 
 -- aim is to prove Lusin's Theorem for the Borel sigma algebra specifically
@@ -37,23 +43,48 @@ class BorelSpace (α : Type*) [TopologicalSpace α] [MeasurableSpace α] : Prop 
     measurable_eq : ‹MeasurableSpace α› = borel α
 -/
 
-/-
-Theorem: the pre-image of a singleton under f measurable is measurable
--/
 
--- write theorem here
 
 /-
-Theorem: the countable union of compact sets is compact.
+Theorem 1: the pre-image of a singleton under f measurable is measurable
 -/
 
--- write theorem here
+-- write Theorem 1 here
+
+
 
 /-
-Proposition 1.2.5 in Cohn's book [Continuity of Measure]: μ(⋃ A_k) = lim μ(A_k) for an increasing sequence of sets {A_k} with A = ⋃ A_k
+Theorem 2: the countable union of compact sets is compact.
 -/
 
--- write proposition here
+-- write Theorem 2 here
+
+
+
+/-
+Proposition 1.2.5 in Cohn's book [continuity of measure]: μ(⋃ A_k) = lim μ(A_k) for an increasing sequence of sets {A_k} with A = ⋃ A_k
+-/
+
+-- write Proposition 1.2.5 here
+
+
+
+/-
+Proposition 7.2.6 in Cohn's book [compactness-supremum characterisation of a set under a regular measure]: let X be a Hausdorff space endowed with the Borel σ-algebra B. Let μ be a regular measure on B.
+If A ∈ B (and A is σ-finite under μ) then μ(A) = sup{μ(K) : K ⊆ A, K compact}.
+-/
+
+-- write Proposition 7.2.6 here
+
+
+
+/-
+Intermediate lemma: if f is a measurable function with K = ⋃K_i for K_i disjoint compact sets, then f is constant on each K_i. Hence, f|K (i.e. f restricted to the set K) continuous.
+-/
+
+-- write intermediate lemma here
+
+
 
 /- Lusin's Theorem for measurable f which takes **countably many values**. Suppose that f takes countable many values {a_1,...,a_k}. Define A_k = f^-1(a_k) for each k, with A = ⋃ A_k.
 
@@ -63,6 +94,22 @@ We now apply the regularity of μ to apply a similar argument, only this time wi
 -/
 
 -- write countable f theorem here
+
+
+
+/-
+From proof in MA359 notes: the sequence of functions f_n := 2^-n * floor(2^n f) converges uniformly to f.
+-/
+
+-- write uniform convergence proof here
+
+
+
+/- Lusin's Theorem for f taking **uncountably many values**:
+-/
+
+-- write uncountable f theorem here
+
 
 
 -- Lusin's Theorem!
